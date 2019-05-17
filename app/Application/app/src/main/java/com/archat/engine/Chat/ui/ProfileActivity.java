945f17b98e58ef403e5a0e510b4d65fd15e04c45
Durva Kapadne <durva@169-231-90-179.wireless.ucsb.edu>
2019-05-17 12:53:19 -0700

    ignore

---
+++ app/Application/app/src/main/java/com/archat/engine/Chat/ui/ProfileActivity.java
@@ -0,0 +1,23 @@
+package com.archat.engine.Chat.ui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.archat.engine.Chat.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+
+}
